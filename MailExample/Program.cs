c00a3c0e8f08916ee44c22beaabdd34b58545e02
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2022-06-17 19:31:36 +0700

    init project smtp

---
+++ MailExample/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Net;
+using System.Net.Mail;
+using System.Net.Mime;
+using System.Text;
+using System.Text.Json;
+
+var smtpClient = new SmtpClient("smtp.ethereal.email")
+{
+    Port = 587,
+    Credentials = new NetworkCredential("<email here>", "<pass here>"),
+    EnableSsl = true,
+};
+
+var attachment = Attachment.CreateAttachmentFromString(JsonSerializer.Serialize(new
+{
+    Message = "Hello World!"
+}), "helloworld.json", Encoding.UTF8, MediaTypeNames.Application.Json);
+
+var message = new MailMessage("fromtest@test.com", "sendertest@test.com")
+{
+    Subject = "Test Email! Hello World!",
+    Body = "<p>Test Email</p><b>Hello World!</b>",
+    IsBodyHtml = true,
+};
+
+message.Attachments.Add(attachment);
+
+try
+{
+    smtpClient.Send(message);
+}
+catch (SmtpException ex)
+{
+    Console.WriteLine(ex.ToString());
+}
